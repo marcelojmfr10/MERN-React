88412e665219902a65e00e242dcbdd09789966f3
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-03-27 18:10:24 -0600

    Calendario en español y generación de modal

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,10 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { CalendarEvent, CalendarModal, NavBar } from "../"
 
-import { addHours, parse, format, startOfWeek, getDay } from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
-import { NavBar } from "../"
-
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
+import { addHours } from 'date-fns'
+import { localizer, getMessagesES } from '../../helpers';
+import { useState } from 'react';
 
 const events = [{
   title: 'Cumpleaños del jefe',
@@ -30,17 +19,60 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+
+    const style = {
+      backgroundColor: '#347cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+    
+    return {
+      style
+    }
+  }
+
+  const onDoubleClick = (event) => {
+
+  }
+
+  const onSelect = (event) => {
+
+  }
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+    setLastView(event);
+  }
+
   return (
     <>
       <NavBar />
 
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
+
+      <CalendarModal />
     </>
   )
 }
---
+++ src/helpers/calendarLocalizer.js
@@ -0,0 +1,18 @@
+
+
+import { dateFnsLocalizer } from 'react-big-calendar';
+import { parse, format, startOfWeek, getDay } from 'date-fns'
+import esES from 'date-fns/locale/es'
+
+const locales = {
+    // 'en-US': enUS,
+    es: esES,
+  }
+
+export const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  });
