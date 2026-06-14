7e6303d00ab6c7759525ef456df5cf50dc22a1aa
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-06-14 15:52:12 -0600

    Guardar en caché archivos estáticos y urls

--- src/sw.js
+++ src/sw.js
@@ -24,10 +24,41 @@ self.skipWaiting();
 
 clientsClaim();
 
+// Implementación manual
 self.addEventListener("install", async (event) => {
   const cache = await caches.open("cache-1");
 
   await cache.addAll([
-    //aca lo que se quiere llevar al cache
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css",
+    "/vite.svg",
   ]);
+});
+
+const apiOfflineFallbacks = [
+  "http://localhost:4000/api/auth/renew",
+  "http://localhost:4000/api/events",
+];
+self.addEventListener("fetch", (event) => {
+  //   console.log(event.request.url);
+  if (!apiOfflineFallbacks.includes(event.request.url)) return;
+
+  const resp = fetch(event.request)
+    .then((response) => {
+      if (!response) {
+        return caches.match(event.request);
+      }
+
+      // guardar en caché la respuesta
+      caches.open("cache-dynamic").then((cache) => {
+        cache.put(event.request, response);
+      });
+      return response.clone();
+    })
+    .catch((err) => {
+      console.log("offline response");
+      return caches.match(event.request);
+    });
+
+  event.respondWith(resp);
 });
